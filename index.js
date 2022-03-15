f78dcaa8d92f158a269a6090b9724793fac43f87
Seaoak <seaoak2003@gmail.com>
2022-03-15 18:22:35 +0900

    Implement analyzing GitHub page

--- index.js
+++ index.js
@@ -51,15 +51,59 @@ function analyzeCatalog(dom, callback) {
         assert(!table[name]); // not twice
         table[name] = url;
     });
+    Object.freeze(table); // just in case
     console.debug(table);
     callback(table);
+}
+
+function analyzeGitHub(dom, callback) {
+    const divElem = dom.querySelectorAll('.Box .Details')[1];
+    assert(divElem);
+    const { filename, file_url } =
+        Array.from(divElem.querySelectorAll('a.Link--primary'))
+            .filter(elem => /^_config\.(yml|json)$/.test(elem.textContent))
+            .reduce((acc, elem) => {
+                assert(!acc); // not twice
+                const filename = elem.textContent;
+                const file_url = elem.href;
+                return { filename, file_url };
+            }, null);
+    const raw_url =
+        file_url.replace('https://github.com/', '/')
+            .replace(/^[/]/, 'https://raw.githubusercontent.com/')
+            .replace('/blob/', '/');
+    const result = { filename, file_url, raw_url };
+    Object.freeze(result); // just in case
+    console.debug(result);
+    callback(result);
 }
 
 function main() {
     fetchHTML(catalog_url, html => {
         parseHTML(html, dom => {
-            analyzeCatalog(dom, table => {
-                console.log(`RESULT: found ${Object.keys(table).length} themes`);
+            analyzeCatalog(dom, themes => {
+                const names = Object.keys(themes);
+                console.log(`${names.length} themes are found in catalog`);
+                const targets = 
+                    names.filter(name => themes[name].startsWith('https://github.com/'))
+                        .slice(0, 1);
+                const table = {};
+                targets.forEach(name => {
+                    const repository_url = themes[name];
+                    fetchHTML(repository_url, html => {
+                        parseHTML(html, dom => {
+                            analyzeGitHub(dom, github_info => {
+                                const { filename, file_url, raw_url } = github_info;
+                                table[name] = { name, repository_url, filename, file_url, raw_url };
+                                if (Object.keys(table).length == targets.length) {
+                                    Object.freeze(table); // just in case
+                                    console.debug(table);
+                                    console.log('${Object.keys(table).length} themes are found in GitHub');
+                                }
+                            });
+                        });
+                    });
+                });
             });
         });
     });
